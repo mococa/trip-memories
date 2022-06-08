110d5206658b39747cb9377dadab1a72dd2c9e27
Luiz Felipe Moureau <xfazxtudo@gmail.com>
2022-06-08 00:24:25 -0300

    backend: updated trip  entity

--- infra/server/entities/trip.go
+++ infra/server/entities/trip.go
@@ -1,10 +1,12 @@
 package entities
 
 type Trip struct {
-	PartitionKey string `json:"partition_key"`
-	SortKey      string `json:"sort_key"`
-	CreatedAt    int    `json:"created_at"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	DoneAt       string `json:"done_at"`
+	PartitionKey string   `json:"partition_key"`
+	SortKey      string   `json:"sort_key"`
+	Picture      string   `json:"picture"`
+	CreatedAt    int      `json:"created_at"`
+	Name         string   `json:"name"`
+	Description  string   `json:"description"`
+	DoneAt       int      `json:"done_at"`
+	Images       []string `json:"images"`
 }
